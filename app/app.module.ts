93a1e85648efe51668555690dc107be7c2958e3f
Dmitry Zimnitsky <dmitryft4@gmail.com>
2018-04-04 14:10:16 +0300

    starting learning Component chapter

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,11 @@ import { ProductComponent } from "./component";
 import { PaAttrDirective } from "./attr.directive";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms"
 import { PaStructureDirective } from "./paif.directive";
+import { ProductTableComponent } from "./productTable.component";
+import { ProductFormComponent } from "./productForm.component";
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-    declarations: [ProductComponent, PaAttrDirective, PaStructureDirective],
+    declarations: [ProductComponent, PaAttrDirective, PaStructureDirective, ProductFormComponent, ProductTableComponent],
     bootstrap: [ProductComponent]
 })
 export class AppModule { }
---
+++ app/productForm.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "paProductForm",
+    template: "<div>{{model}}</div>"
+})
+export class ProductFormComponent {
+    model: string = "This is the model";
+}
