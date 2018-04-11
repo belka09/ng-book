808287284692df7171d53bb5be2d182841e913b5
dzimnitsky <dzimnitsky@dev-team.com>
2018-04-11 15:36:33 +0300

    start learning services

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,10 +9,12 @@ import { ProductFormComponent } from "./productForm.component";
 import { paToggleView } from "./toggleView.component";
 import { PaAddTaxPipe } from "./addTax.pipe";
 import { PaCategoryFilterPipe } from "./categoryFilter.pipe";
+import { PaDiscountDisplayComponent } from "./discountDisplay.component";
+import { PaDiscountEditorComponent } from "./discountEditor.component";
 // import { LOCALE_ID } from "@angular/core";
 @NgModule({
     imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-    declarations: [ProductComponent, PaAttrDirective, PaStructureDirective, ProductFormComponent, ProductTableComponent, paToggleView, PaAddTaxPipe, PaCategoryFilterPipe],
+    declarations: [ProductComponent, PaAttrDirective, PaStructureDirective, ProductFormComponent, ProductTableComponent, paToggleView, PaAddTaxPipe, PaCategoryFilterPipe,PaDiscountDisplayComponent, PaDiscountEditorComponent],
     // providers: [{provide: LOCALE_ID, useValue: "fr-FR"}],
     bootstrap: [ProductComponent]
 })
